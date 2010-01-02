08c6bdb8532e57b5e9a8455a0fd2ae4b7e88339a
Francisco José Seva mora <franciscojseva@gmail.com>
2010-01-03 00:54:36 +0100

    Distribución de alphas.
    Signed-off-by: Francisco José Seva mora <franciscojseva@gmail.com>

--- RobustHiding_PID/src/es/us/pid/grupo14/impl/EmbeddingAlgorithmG8Impl.java
+++ RobustHiding_PID/src/es/us/pid/grupo14/impl/EmbeddingAlgorithmG8Impl.java
@@ -8,6 +8,10 @@ import ij.process.ImageProcessor;
 import es.us.pid.grupo14.EmbeddingAlgorithm;
 
 public class EmbeddingAlgorithmG8Impl implements EmbeddingAlgorithm {
+  
+  //Arrays para guardar las frecuencias para el histograma
+	Hashtable alphasBefore = new Hashtable();
+  Hashtable alphasAfter = new Hashtable();
 
 	@Override
 	public ImagePlus embedBits(ImagePlus img, byte[] bits, int t, int g, int m, int n,
@@ -29,6 +33,13 @@ public class EmbeddingAlgorithmG8Impl implements EmbeddingAlgorithm {
 		for (int i = 0; i < h; i = i + m) {
 			for (int j = 0; j < w; j = j + n) {
 				int alpha = getAlpha(matrixM, pixels, i, j, delta);
+				
+				if (!alphasBefore.containsKey(alpha)){
+  			  alphasBefore.set(alpha, 1);
+  			}
+				//voy guardando la frecuencia acumulada
+				alphasBefore.put(alpha, alphasBefore.get(alpha) + 1);
+				
 				res = createGap(res, pixels, beta1, alpha, t, delta, i, j, m, n);
 
 				//si el alpha esta en rango y quedan aun datos por insertar
@@ -38,7 +49,30 @@ public class EmbeddingAlgorithmG8Impl implements EmbeddingAlgorithm {
 					bitCont++;
 				}
 			}
-
+			
+			
+			/** TODO: Esto debería de estar en una fución a parte.
+			 * Calculo la frecuencia de los distintos valores de alpha una
+			 * vez se ha realizado la inyección
+			 **/
+			// TODO cuidado con como me devuelve el array de enteros
+  		int[][] pixelsStego = res.getProcessor().getIntArray();
+			//Mientras queden bloques (con la stego-imagen)
+			for( int i = 0; i < h; i = i + m){
+			  for (int j = 0; j < w; j = j + n) {
+		      int alphaStego = getAlpha(matrixM, pixelsStego, i, j, delta);
+  				//voy guardando la frecuencia acumulada
+  				if (!alphasAfter.containsKey(alphaStego)){
+  				  alphasAfter.set(alphaStego, 1);
+  				}
+  				alphasAfter.put(alphaStego, alphasAfter.get(alphaStego) + 1);
+		    }
+			}
+			
+			//Muesto los dos histogramas en pantalla nada más terminar con la inyección
+			getAlphaDist(alphasBefore, "Distribución antes de inyectar los datos");
+			getAlphaDist(alphasAfter, "Distribución después de inyectar los datos");
+			
 		}
 
 		return res;
@@ -217,4 +251,53 @@ public class EmbeddingAlgorithmG8Impl implements EmbeddingAlgorithm {
 			return true;
 		}
 	}
+
+  /**
+   * getAlphaDist
+   *
+   * @param  int [] alphas Valores que toma alpha en la distribución y el número
+   *          de veces que aparece.
+   * @param String title. Título de la ventana con el histograma.
+   **/
+  public void getAlphaDist(Hashtable alphas, String title){
+    
+     //arrays que cargan los datos en la distribución.
+     float [] x = new float [alphas.size()];
+     float [] y = new float [alphas.size()];
+     int pos=0;
+     float maxFreq=0, minFreq=Float.MAX_VALUE;
+     
+     //ordeno la hashtable
+     Vector vector = new Vector(alphas.keySet());
+     Collections.sort(vector);
+     
+     //añado los elementos de la hashtable al x e y
+     for (Enumeration e = vector.elements(); e.hasMoreElements();) {
+       float key = (Float)e.nextElement();
+       float value = (Float)alphas.get(key);
+       x[pos] = key;
+       y[pos] = value;
+       if(value > maxFreq){
+         maxFreq = value;
+       }
+       if(value < minFreq){
+         minFreq = value;
+       }
+       pos++;
+     }
+     
+     float Xmax = (Float) vector.lastElement() + 1, Xmin = (Float) vector.firtsElement() - 1;
+     float Ymax =maxFreq + 1, Ymin=minFreq - 1;
+     PlotWindow plot = new PlotWindow(title, 
+                                      "Valores de alpha", 
+                                      "Número de alphas",
+                                       x,
+                                       y); 
+     plot.addPoints(x,y,PlotWindow.CIRCLE);
+     plot.setLimits(Xmin, Xmax, Ymin, Ymax);
+     plot.setColor(Color.blue);
+     plot.draw();
+
+  }
+  
 }
