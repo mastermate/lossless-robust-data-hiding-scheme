1a1d463d6bbab142dff4611b9457cf887b5c2e53
Francisco José Seva mora <franciscojseva@gmail.com>
2010-01-03 13:37:08 +0100

    Plugin de inyección. Versión 0.1 (lista para testear inyección con imágenes no cuadradas no JPG)
    Signed-off-by: Francisco José Seva mora <franciscojseva@gmail.com>

--- RobustHiding_PID/src/RobustHiding_PID.java
+++ RobustHiding_PID/src/RobustHiding_PID.java
@@ -1,7 +1,10 @@
+import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
-
+import es.us.pid.grupo14.impl.EmbeddingAlgorithmG8Impl;
+import es.us.pid.grupo14.impl.EmbeddedValidationG8Impl;
+import java.io.IOException;
 
 public class RobustHiding_PID implements PlugInFilter {
 
